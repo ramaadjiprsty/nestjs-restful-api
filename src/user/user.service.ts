1e21ccd730fca3112cb088410446dbaa06a8b7cb
Rama <ramaadjiprsty@gmail.com>
2024-12-24 21:02:08 +0700

    feat(user): setup validation when username already used and hashing the password using bcrypt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,11 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, Inject, Injectable } from '@nestjs/common';
 import { RegisterUserRequest, UserResponse } from '../model/user.model';
 import { ValidationService } from '../common/validation.service';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 import { PrismaService } from '../common/prisma.service';
 import { UserValidation } from './user.validation';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -24,6 +25,19 @@ export class UserService {
       },
     });
 
-    return null;
+    if (totalUserWithSameUsername != 0) {
+      throw new HttpException('Username already exist', 400);
+    }
+
+    registerRequest.password = await bcrypt.hash(registerRequest.password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: registerRequest,
+    });
+
+    return {
+      username: user.username,
+      name: user.name,
+    };
   }
 }
