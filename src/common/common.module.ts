c01b4be233504b26f33a9c17dab3ae968b1edc1e
Enneva <ramaadjiprsty@gmail.com>
2024-12-24 14:41:04 +0700

    fet: setup winston for app logging

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
-@Module({})
+@Global()
+@Module({
+  imports: [
+    WinstonModule.forRoot({
+      format: winston.format.json(),
+      transports: [new winston.transports.Console()],
+    }),
+  ],
+})
 export class CommonModule {}
