154e8e7da86417ffb9c36dc903043aac8db25faf
Rama <ramaadjiprsty@gmail.com>
2024-12-24 22:21:06 +0700

    feat: create error filter for response

---
+++ test/user.spec.ts
@@ -0,0 +1,39 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { Logger } from 'winston';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+  let logger: Logger;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    logger = app.get(WINSTON_MODULE_PROVIDER);
+  });
+
+  describe('POST /api/users', () => {
+    it('should be rejected if request is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users')
+        .send({
+          username: '',
+          password: '',
+          name: '',
+        });
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors).toBeDefined();
+    });
+  });
+});
