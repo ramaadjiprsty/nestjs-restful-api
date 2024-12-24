151069a177215ff11a57651dcd00241d6ee862f7
Rama <ramaadjiprsty@gmail.com>
2024-12-24 21:12:09 +0700

    feat(user): create register post method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { RegisterUserRequest, UserResponse } from '../model/user.model';
+import { WebResponse } from '../model/web.model';
 
-@Controller('api/users')
-export class UserController {}
+@Controller('/api/users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  async register(
+    @Body() request: RegisterUserRequest,
+  ): Promise<WebResponse<UserResponse>> {
+    const result = await this.userService.register(request);
+    return {
+      data: result,
+    };
+  }
+}
