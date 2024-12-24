4e8d300b39b340976b02de1af080fede7137b4c1
Rama <ramaadjiprsty@gmail.com>
2024-12-24 20:57:58 +0700

    feat(user): add user validation for register

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { RegisterUserRequest, UserResponse } from '../model/user.model';
+import { ValidationService } from '../common/validation.service';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+import { PrismaService } from '../common/prisma.service';
+import { UserValidation } from './user.validation';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    private validationService: ValidationService,
+    @Inject(WINSTON_MODULE_PROVIDER) private logger: Logger,
+    private prismaService: PrismaService,
+  ) {}
+  async register(request: RegisterUserRequest): Promise<UserResponse> {
+    this.logger.info(`Register new user ${JSON.stringify(request)}`);
+    const registerRequest: RegisterUserRequest =
+      this.validationService.validate(UserValidation.REGISTER, request);
+
+    const totalUserWithSameUsername = await this.prismaService.user.count({
+      where: {
+        username: registerRequest.username,
+      },
+    });
+
+    return null;
+  }
+}
